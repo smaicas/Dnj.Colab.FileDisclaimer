e13589094046b53e2fcf71c30855a9f499207513
smaicas <samuel.maicas.development@gmail.com>
2022-11-27 22:42:20 +0100

    Format && Disclaimer

--- Dnj.Colab.Samples.FileDisclaimer/Generate-MSIX-net7.0-windows10.0.19041.0.ps1
+++ Dnj.Colab.Samples.FileDisclaimer/Generate-MSIX-net7.0-windows10.0.19041.0.ps1
@@ -1,1 +1,1 @@
-dotnet publish -f net7.0-windows10.0.19041.0 -c Release /p:RuntimeIdentifierOverride=win10-x64 --version 1.0.0.2
+dotnet publish -f net7.0-windows10.0.19041.0 -c Release /p:RuntimeIdentifierOverride=win10-x64
--- Dnj.Colab.Samples.FileDisclaimer/ViewModels/AddFileDisclaimerVm.cs
+++ Dnj.Colab.Samples.FileDisclaimer/ViewModels/AddFileDisclaimerVm.cs
@@ -1,14 +1,31 @@
-﻿using System.Text;
+﻿/* This file is copyright © 2022 Dnj.Colab repository authors.
+
+Dnj.Colab content is distributed as free software: you can redistribute it and/or modify it under the terms of the General Public License version 3 as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Dnj.Colab content is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the General Public License version 3 for more details.
+
+You should have received a copy of the General Public License version 3 along with this repository. If not, see <https://github.com/smaicas-org/Dnj.Colab/blob/dev/LICENSE>. */
+
+using System.Text;
 using Dnj.Colab.Samples.FileDisclaimer.Abstractions;
 using Dnj.Colab.Samples.FileDisclaimer.RCL.ViewModels;
 
 namespace Dnj.Colab.Samples.FileDisclaimer.ViewModels;
+
 public class AddFileDisclaimerVm : IAddFileDisclaimerVm
 {
     private readonly IFolderPicker _folderPicker;
 
-    public AddFileDisclaimerVm(IFolderPicker folderPicker) => _folderPicker = folderPicker ?? throw new ArgumentNullException(nameof(folderPicker));
-    public async Task SelectDestinyFolder() => DestinyFolder = await _folderPicker.PickFolder();
+    public AddFileDisclaimerVm(IFolderPicker folderPicker)
+    {
+        _folderPicker = folderPicker ?? throw new ArgumentNullException(nameof(folderPicker));
+    }
+
+    public async Task SelectDestinyFolder()
+    {
+        DestinyFolder = await _folderPicker.PickFolder();
+    }
+
     public string? DestinyFolder { get; set; }
     public string? DisclaimerText { get; set; } = Messages.DisclaimerText;
     public bool Loading { get; set; }
@@ -25,6 +42,22 @@ public class AddFileDisclaimerVm : IAddFileDisclaimerVm
         return res;
     }
 
+    public FileExtensionComment[] AllowedFileExtensions { get; set; } =
+    {
+        new()
+        {
+            Value = "cs",
+            CommentOpen = "/* ",
+            CommentClose = " */"
+        },
+        new()
+        {
+            Value = "razor",
+            CommentOpen = "@* ",
+            CommentClose = " *@"
+        }
+    };
+
     private async Task<AddFileDisclaimerResult> ProcessDisclaimGeneration()
     {
         if (DestinyFolder == default) return Ko(nameof(DestinyFolder));
@@ -32,7 +65,8 @@ public class AddFileDisclaimerVm : IAddFileDisclaimerVm
         int processedFiles = default;
         foreach (FileExtensionComment allowedFileExtension in AllowedFileExtensions)
         {
-            string[] files = Directory.GetFiles(DestinyFolder, $"*.{allowedFileExtension.Value}", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(DestinyFolder, $"*.{allowedFileExtension.Value}",
+                SearchOption.AllDirectories);
             foreach (string file in files)
             {
                 StringBuilder sb = new(allowedFileExtension.CommentOpen);
@@ -55,34 +89,21 @@ public class AddFileDisclaimerVm : IAddFileDisclaimerVm
         return Ok(processedFiles);
     }
 
-    private static AddFileDisclaimerResult Ok(int processedFileNumber) =>
-        new()
+    private static AddFileDisclaimerResult Ok(int processedFileNumber)
+    {
+        return new AddFileDisclaimerResult
         {
             State = AddFileDisclaimerResultState.OK,
             ProcessedFileNumber = processedFileNumber
         };
+    }
 
-    private static AddFileDisclaimerResult Ko(string error) =>
-        new()
+    private static AddFileDisclaimerResult Ko(string error)
+    {
+        return new AddFileDisclaimerResult
         {
             State = AddFileDisclaimerResultState.KO,
             Error = $"Empty {error}"
         };
-
-    public FileExtensionComment[] AllowedFileExtensions { get; set; } = new FileExtensionComment[]
-    {
-        new FileExtensionComment()
-        {
-            Value = "cs",
-            CommentOpen = "/* ",
-            CommentClose = " */"
-        },
-        new FileExtensionComment()
-        {
-            Value = "razor",
-            CommentOpen = "@* ",
-            CommentClose = " *@"
-        }
-    };
-
+    }
 }
